0198e438fa5db8f6f0e2f3883116c622bb1ffe82
Koa <yl@koala.one>
2021-07-12 06:32:03 +0000

    added header

--- src/controller/posts.ts
+++ src/controller/posts.ts
@@ -2,5 +2,6 @@ import { get } from "../redis";
 
 export const posts = async (req, res) => {
   const jres = await get("posts", ".");
+  res.setHeader('Content-Type', 'application/json');
   res.send(jres);
 };
