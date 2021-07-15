f095d7efed50c61fc7718dc9b5b814e15c23b8f0
Koa <yl@koala.one>
2021-07-15 19:44:22 +0100

    :(

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import * as express from "express";
 import { posts } from "./controller/posts";
-import { client, set } from "./redis";
+import { client, arrappend, get } from "./redis";
+import { v4 as uuidv4 } from "uuid";
+
+const tokenTest = async () => {
+  for (let i = 0; i < 10; i++) {
+    await client.arrappend("tokens", [JSON.stringify(uuidv4())], ".");
+  }
+  console.log(await get("tokens", "."));
+};
 
 const main = async () => {
   await client.connect();
@@ -8,7 +16,7 @@ const main = async () => {
   const port = 8080;
 
   app.get("/posts", posts);
-
+  tokenTest();
   app.listen(port);
 };
 
--- src/redis.ts
+++ src/redis.ts
@@ -16,3 +16,7 @@ export const set = async (k, p, v) => {
 export const get = async (k, p) => {
   return await client.get(k, p);
 };
+
+export const arrappend = async (k, p, j) => {
+  return await client.arrappend(k, p, j);
+};
