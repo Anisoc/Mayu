bb6d106e4cde0c39e349dfdf83dbb716aa8f06b9
Yoav Levi <yl@koala.one>
2021-08-26 15:57:37 +0000

    fill in mock data

--- src/init/users.ts
+++ src/init/users.ts
@@ -9,7 +9,8 @@ export const usersInit = async () => {
       id: uuidv4(),
       email: `admin@test.com`,
       username: `admin`,
-      password: "",
+      password: "$2b$10$B7OqGcu3AQ7QpeiZB8c3sOVQBij2PEIr7TcdklM.KOM8YsAoz/Uz.",
+      createdAt: "2021-08-26T14:43:49.429Z",
       admin: true,
     };
     await arrappend("users", ".", [JSON.stringify(obj)]);
