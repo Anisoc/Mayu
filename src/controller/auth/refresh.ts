300557f3cf12a014d6a448b9ed8e05f460406e92
Yoav Levi <yl@koala.one>
2021-08-26 15:56:47 +0000

    moved user object into a variable

--- src/controller/auth/refresh.ts
+++ src/controller/auth/refresh.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
-import { getUserById, getToken } from "@redis";
+import { getUserById, getToken, arrappend, delToken } from "@redis";
 import jwt from "jsonwebtoken";
 
 export const refresh: RequestHandler = async (req, res) => {
@@ -8,29 +8,33 @@ export const refresh: RequestHandler = async (req, res) => {
       return res.sendStatus(403);
     }
 
-    let user = await getUserById(token.sub);
-    console.log(token);
-    console.log(user);
+    const users = await getUserById(token.sub);
+    const t = await getToken(req.body.refreshToken);
 
-    if (user && getToken(token)) {
-      const at = jwt.sign({ sub: user[0].id }, process.env.SEED, {
+    if (users.length == 1 && !(t instanceof Array)) {
+      const user = users[0];
+
+      const at = jwt.sign({ sub: user.id }, process.env.SEED, {
         expiresIn: "120m",
       });
 
-      const rt = jwt.sign({ sub: user[0].id }, process.env.SEED, {
+      const rt = jwt.sign({ sub: user.id }, process.env.SEED, {
         expiresIn: "7d",
       });
 
+      await arrappend("tokens", ".", [JSON.stringify(rt)]);
+      await delToken(t);
+
       return res.json({
-        id: user[0].id,
-        email: user[0].email,
-        username: user[0].username,
-        createdAt: user[0].createdAt,
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        createdAt: user.createdAt,
         accessToken: at,
         refreshToken: rt,
       });
     }
 
-    return res.status(403);
+    return res.sendStatus(403);
   });
 };
