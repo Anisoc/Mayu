d66f6f740b9cef452ff3a33da3d3bf162ea44a55
Yoav Levi <yl@koala.one>
2021-08-26 17:20:46 +0000

    format

--- src/controller/auth/refresh.ts
+++ src/controller/auth/refresh.ts
@@ -1,40 +1,44 @@
 import { RequestHandler } from "express";
-import { getUserById, getToken, arrappend, delToken } from "@redis";
+import { getUserById, getToken, arrappend, delToken } from "database/redis";
 import jwt from "jsonwebtoken";
 
 export const refresh: RequestHandler = async (req, res) => {
-  jwt.verify(req.body.refreshToken, process.env.SEED, async (err, token) => {
-    if (err) {
+  await jwt.verify(
+    req.body.refreshToken,
+    process.env.SEED,
+    async (err, token) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      const users = await getUserById(token.sub);
+      const t = await getToken(req.body.refreshToken);
+
+      if (users.length == 1 && !(t instanceof Array)) {
+        const user = users[0];
+
+        const at = jwt.sign({ sub: user.id }, process.env.SEED, {
+          expiresIn: "120m",
+        });
+
+        const rt = jwt.sign({ sub: user.id }, process.env.SEED, {
+          expiresIn: "7d",
+        });
+
+        await arrappend("tokens", ".", [JSON.stringify(rt)]);
+        await delToken(t);
+
+        return res.json({
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          createdAt: user.createdAt,
+          accessToken: at,
+          refreshToken: rt,
+        });
+      }
+
       return res.sendStatus(403);
     }
-
-    const users = await getUserById(token.sub);
-    const t = await getToken(req.body.refreshToken);
-
-    if (users.length == 1 && !(t instanceof Array)) {
-      const user = users[0];
-
-      const at = jwt.sign({ sub: user.id }, process.env.SEED, {
-        expiresIn: "120m",
-      });
-
-      const rt = jwt.sign({ sub: user.id }, process.env.SEED, {
-        expiresIn: "7d",
-      });
-
-      await arrappend("tokens", ".", [JSON.stringify(rt)]);
-      await delToken(t);
-
-      return res.json({
-        id: user.id,
-        email: user.email,
-        username: user.username,
-        createdAt: user.createdAt,
-        accessToken: at,
-        refreshToken: rt,
-      });
-    }
-
-    return res.sendStatus(403);
-  });
+  );
 };
